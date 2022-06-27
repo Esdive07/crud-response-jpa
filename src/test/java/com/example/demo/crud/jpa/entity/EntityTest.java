7b3e55681049dd1f688887e81b7476946ad134ad
robinarehen <robinarehen@gmail.com>
2022-06-27 16:21:23 +0200

    adición de test para direcciones funciona ok

--- src/test/java/com/example/demo/crud/jpa/entity/EntityTest.java
+++ src/test/java/com/example/demo/crud/jpa/entity/EntityTest.java
@@ -12,10 +12,12 @@ import org.junit.jupiter.api.Test;
 public class EntityTest {
 
 	private UserEntity userEntity;
+	private DireccionEntity direccionEntity;
 
 	@BeforeEach
 	public void init() {
 		this.userEntity = new UserEntity();
+		this.direccionEntity = new DireccionEntity();
 	}
 
 	@Test
@@ -44,7 +46,33 @@ public class EntityTest {
 		assertEquals(documento, this.userEntity.getDocumento());
 		assertNotNull(this.userEntity.getDireccionEntities());
 		assertNotNull(this.userEntity.getDatosContactoEntities());
+	}
 
+	@Test
+	public void direccionEntityTest() {
+		Integer id = 1;
+		String calle = "General";
+		String localidad = "carabanchel";
+		Integer numero = 69;
+		String puerta = "1e";
+		Integer codigoPostal = 28019;
+		UserEntity userEntity = new UserEntity();
+
+		this.direccionEntity.setId(id);
+		this.direccionEntity.setCalle(calle);
+		this.direccionEntity.setLocalidad(localidad);
+		this.direccionEntity.setNumero(numero);
+		this.direccionEntity.setPuerta(puerta);
+		this.direccionEntity.setCodigoPostal(codigoPostal);
+		this.direccionEntity.setUserEntity(userEntity);
+
+		assertEquals(id, this.direccionEntity.getId());
+		assertEquals(calle, this.direccionEntity.getCalle());
+		assertEquals(localidad, this.direccionEntity.getLocalidad());
+		assertEquals(numero, this.direccionEntity.getNumero());
+		assertEquals(puerta, this.direccionEntity.getPuerta());
+		assertEquals(codigoPostal, this.direccionEntity.getCodigoPostal());
+		assertEquals(userEntity, this.direccionEntity.getUserEntity());
 	}
 
 }
